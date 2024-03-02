134ba04b9d20de64b41a82b1415abc1a788985e4
abhijithreddy12 <120442172+abhijithreddy12@users.noreply.github.com>
2024-03-02 08:40:09 +0530

    Add files via upload

---
+++ vowel r consonant.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cctype> 
+
+using namespace std;
+
+int main() {
+    char ch;
+
+    cout << "Enter a character: ";
+    cin >> ch;
+
+    ch = tolower(ch);
+
+    if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+        cout << ch << " is a vowel." << endl;
+    } else if (ch >= 'a' && ch <= 'z') {
+        cout << ch << " is a consonant." << endl;
+    } else {
+        cout << "Invalid input. Please enter an alphabet." << endl;
+    }
+
+    return 0;
+}
